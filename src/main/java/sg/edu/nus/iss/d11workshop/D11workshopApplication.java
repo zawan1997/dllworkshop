49a71e67ccacab3f1a5e80c35b3b11850a02d67c
zawan1997 <nawaz.shss@gmail.com>
2022-07-18 13:47:42 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/d11workshop/D11workshopApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D11workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D11workshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/d11workshop/D11workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.d11workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D11workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
